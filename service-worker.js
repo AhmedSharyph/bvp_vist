4cf05f57b6bcfd55bfb50c8267ffaa2dd93c9229
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2026-01-03 19:35:18 +0500

    Remove unused event listeners from service worker
    Removed unused service worker event listeners for install and fetch.

--- service-worker.js
+++ service-worker.js
@@ -4,21 +4,7 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./register.html",
   "./visit.html",
-  "./style.css",
+  "./style.css",  // optional if you have it
   "./app.js",
   "./manifest.json"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
-});
